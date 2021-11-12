e86de6a7125fb9ca3409bfb4496a3932070a8afd
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-12 15:35:16 +0100

    added images to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true
-  # validates :name, uniqueness: true, presence: true
+  has_one_attached :image
 end
